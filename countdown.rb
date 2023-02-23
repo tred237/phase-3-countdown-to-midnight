b2af40a1d3792544ac8bcca2fb2b4d4067289874
trevor dewindt <trevor.dewindt@gmail.com>
2023-02-22 19:25:13 -0700

    commit tree d5f532774965354b88325cbdb902649e36471cc6

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep s = 1
+    sleep(s)
 end
+
+def countdown start_num
+    while start_num > 0 
+        puts "#{start_num} SECOND(S)!"
+        start_num -= 1
+        countdown_with_sleep
+    end
+    "HAPPY NEW YEAR!"
+end
+
+countdown 10
